817b416dd11edc9779507a0222e0b862132e8035
morriskaria <morriskaria542@gmail.com>
2025-08-05 07:52:04 +0300

    commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
-
-def square_integers(int_list):
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+def reverse_string(str):
+    reversed_str = ""
+    for char in str:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+def square_integers(int_list):
+    return [num ** 2 for num in int_list]
